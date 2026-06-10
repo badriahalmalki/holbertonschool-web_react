207779d1ec73492fb034905a9d4473deaf078241
badriahalmalki <badryahbalmalki@gmail.com>
2026-06-10 12:21:50 +0300

    fix multi bugs

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -41,24 +41,21 @@ export const createEmployee = function(salary: number | string): Director | Teac
 	}
 }
 
-export function isDirector(employee: DirectorInterface | TeacherInterface): boolean {
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
 	return (employee instanceof Director);
   }
 
 export function executeWork(employee: DirectorInterface | TeacherInterface): string {
-if (employee instanceof Director) {
-	return (employee.workDirectorTasks());
-} else if (employee instanceof Teacher){
-	return(employee.workTeacherTasks());
-}
+  if (employee instanceof Director) {
+    return employee.workDirectorTasks();
+  } else {
+    return (employee as Teacher).workTeacherTasks();
+  }
 }
 
 type Subjects = "Math" | "History";
 
-export function teachClass(todayClass:Subjects): string {
-  if (todayClass === "Math") {
-    return `Teaching Math`;
-  } else if (todayClass === "History") {
-    return `Teaching History`;
-  }
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") return "Teaching Math";
+  return "Teaching History"; // covers all cases since type is "Math" | "History"
 }
