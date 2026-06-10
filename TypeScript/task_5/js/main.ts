e661200b4c7d2765e414244aa9fdbf1192019b6f
badriahalmalki <badryahbalmalki@gmail.com>
2026-06-10 12:43:01 +0300

    adding sumMinorCredits func

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -1,8 +1,14 @@
 interface MajorCredits {
-	credit: number,
+	credits: number,
 	brand: "MajorCredits"
   }
 interface MinorCredits {
-	credit: number,
+	credits: number,
 	brand: "MinorCredits"
   }
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits, brand: "MajorCredits" };
+}
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits, brand: "MinorCredits" };
+}
